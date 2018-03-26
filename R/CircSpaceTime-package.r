77b2022f922d4d7e96304d19a617466b0b1d7637
santoroma <santoro.ma@gmail.com>
2018-03-26 13:33:07 +0200

    ad a package description in man

---
+++ R/CircSpaceTime-package.r
@@ -0,0 +1,9 @@
+#' CircSpaceTime.
+#'
+#' @name CircSpaceTime
+#' @docType package
+#' This package implements spatial, spatio-temporal and temporal models for circular data.
+#' Currently the following models are implemented:
+#'  Spatial Wrapped Normal
+#'  We are going to update constantly the library with Bayesian and classical models dealing with complex dependence structures for circular, cylindrical and spherical variable.
+NULL
